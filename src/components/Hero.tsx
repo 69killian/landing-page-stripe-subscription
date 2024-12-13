b3a6cca1e961aed93d55929586e497ae32eb0381
Killian <martykillian7@gmail.com>
2024-12-13 15:59:39 +0100

    Hero Section and Overflow Hidden

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,22 +11,18 @@ export const Hero = () => {
   const [loaded, setLoaded] = useState(false);
   const [scrollY, setScrollY] = useState(0);
 
-
   const handleScroll = () => {
     setScrollY(window.scrollY);
   };
 
   useEffect(() => {
-   
     window.addEventListener("scroll", handleScroll);
 
-    
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
- 
   const tilt = Math.min(scrollY / 10, 30); // Limite l'inclinaison à 30 degrés
 
   const handleImageLoad = () => {
@@ -42,35 +38,36 @@ export const Hero = () => {
       <section className="mb-10 mt-[-40px] container grid place-items-center text-center py-20 md:py-32 gap-10 font-sans">
         {/* Texte principal */}
         <div className="space-y-6">
-          <main className="text-5xl md:text-6xl font-bold">
-            <button className="mb-10 relative inline-flex h-9 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-blue-900 transition-all duration-300 hover:bg-transparent px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                NextJS technology for better integrations &gt;
-              </span>
-            </button>
+          <main className="text-4xl sm:text-5xl md:text-6xl font-bold">
+          <button className="mb-10 relative inline-flex h-9 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-sm sm:text-base mt-10 sm:mt-6">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-blue-900 transition-all duration-300 hover:bg-transparent px-3 py-1 text-sm sm:text-xs md:text-sm font-medium text-white backdrop-blur-3xl">
+              NextJS technology for better integrations &gt;
+            </span>
+          </button>
 
-            <h1 className="font-sans">
+
+            <h1 className="font-sans text-3xl sm:text-4xl md:text-5xl">
               Your{" "}
               <span className="bg-gradient-to-r from-[#2B65E2] via-[#537DE5] to-[#2B65E280] text-transparent bg-clip-text">
                 Stripe
               </span>{" "}
               Integration In
             </h1>
-            <h2>
-              <span className="text-[80px] text-white bg-blue-900 rounded-[10px] px-5 inline-block transform rotate-6 transition-transform duration-300 hover:rotate-0">
+            <h2 className="text-4xl sm:text-5xl md:text-6xl">
+              <span className="text-[50px] sm:text-[60px] md:text-[80px] text-white bg-blue-900 rounded-[10px] px-5 inline-block transform rotate-6 transition-transform duration-300 hover:rotate-0">
                 Minutes
               </span>
             </h2>
           </main>
 
-          <p className="text-xl text-muted-foreground md:w-10/12 mx-auto font-sans">
+          <p className="text-lg  text-muted-foreground  mx-auto font-sans">
             Stripe integrations are intimidating, but they don&apos;t have to be. Let&apos;s prove it.
           </p>
 
           {/* Boutons */}
           <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4">
-            <Button className="w-full md:w-auto bg-blue-700 hover:bg-blue-800">
+            <Button className="w-full md:w-auto bg-blue-700 hover:bg-blue-800 text-sm sm:text-base">
               Get Started
             </Button>
 
@@ -86,20 +83,25 @@ export const Hero = () => {
           </div>
         </div>
 
-
-        <LogoMarquee/>
+        <LogoMarquee />
 
         {/* Image */}
-        <div style={{transform: `perspective(1500px) rotateX(${tilt}deg)`, WebkitMaskImage: "linear-gradient(to bottom, black 80%, transparent 100%)",}} className={` shadow relative z-10 mt-0 p-5 bg-gray-100 rounded-xl border-2 border-blue-300/20 backdrop-blur-sm bg-blue-100/10 dark:border-blue-900/20 dark:bg-blue-900/20 ${
-              loaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
-            }`} >
+        <div
+          style={{
+            transform: `perspective(1500px) rotateX(${tilt}deg)`,
+            WebkitMaskImage: "linear-gradient(to bottom, black 80%, transparent 100%)",
+          }}
+          className={`shadow relative z-10 mt-0 p-5 bg-gray-100 rounded-xl border-2 border-blue-300/20 backdrop-blur-sm bg-blue-100/10 dark:border-blue-900/20 dark:bg-blue-900/20 ${
+            loaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
+          }`}
+        >
           <div className="absolute inset-0 rounded-md pointer-events-none"></div>
           <Image
             src="/dashboardstripeeng.png"
             width={1320}
             height={512}
             alt="Stripe Course Illustration"
-            className={` rounded-md select-none pointer-events-none transition-all duration-700 ${
+            className={`rounded-md select-none pointer-events-none transition-all duration-700 ${
               loaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
             }`}
             style={{
